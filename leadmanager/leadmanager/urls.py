dfea253d2922a388d27fd2329cd11164970ceba7
ckd1618 <ckd1618@gmail.com>
2019-03-12 23:02:33 -0700

    problems with file name having capital letters

--- leadmanager/leadmanager/urls.py
+++ leadmanager/leadmanager/urls.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    #make sure your frontend loads before the leads
+    path('', include('frontend.urls')),
     path('', include('leads.urls')),
 ]
